f6e3947ca09fd6ad5fa52b4ed2c3e5974f12fbff
laoyang <super9208@live.cn>
2019-07-15 18:26:32 +0800

    create,edit and delete item by form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
 import click
@@ -13,28 +13,30 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'dev'
+
 
 class User(db.Model):
     """用户模型"""
-    id = db.Column(db.INTEGER,primary_key=True)
-    name = db.Column(db.String(32)) #名字
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(32))  # 名字
+
 
 class Movie(db.Model):
     """电影模型"""
-    id = db.Column(db.INTEGER, primary_key=True) # 主键
+    id = db.Column(db.INTEGER, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
 
-
-
-@app.cli.command() # 注册为命令
-@click.option('--drop',is_flag = True,help='Create after drop.')
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')
 def initdb(drop):
     if drop:
         db.drop_all()
     db.create_all()
-    click.echo("Initialized database.") #输出提示信息
+    click.echo("Initialized database.")  # 输出提示信息
+
 
 @app.cli.command()
 def forge():
@@ -53,11 +55,12 @@ def forge():
     ]
     user = User(name=name)
     for m in movies:
-        movie = Movie(title=m['title'],year=m['year'])
+        movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
     db.session.add(user)
     db.session.commit()
     click.echo('Done')
+
 
 @app.context_processor
 def inject_user():
@@ -65,17 +68,54 @@ def inject_user():
     user = User.query.first()
     return dict(user=user)
 
+
 @app.errorhandler(404)
-def page_not_found(e): # e异常对象
+def page_not_found(e):  # e异常对象
     """404页面"""
     user = User.query.first()
-    return render_template("404.html"),404
+    return render_template("404.html"), 404
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    user = User.query.first()
+    if request.method == "POST":
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 50:
+            flash("Invalid input")
+            return redirect(url_for("index"))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash("Item created.")
+        return redirect(url_for("index"))
+
     movies = Movie.query.all()
     return render_template("index.html", movies=movies)
+
+
+@app.route("/movies/<int:movie_id>", methods=["GET", "POST", "DELETE"])
+def update_movie(movie_id):
+    """更新或者是获取一个movie"""
+    if request.method == "GET":
+        movie = Movie.query.get_or_404(movie_id)
+        return render_template("edit.html", movie=movie)
+    elif request.method == "POST":
+        title = request.form.get("title")
+        year = request.form.get("year")
+        if not title or not year or len(year) > 4 or len(title) > 50:
+            flash("Invalid input")
+            return redirect(url_for("update_movie", movie_id=movie_id))
+        movie = Movie.query.get_or_404(movie_id)
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash("Item updated.")
+        return redirect(url_for("index"))
+    elif request.method == "DELETE":
+        movie = Movie.query.get_or_404(movie_id)
+        db.session.remove(movie)
+        db.session.commit()
 
 
 @app.route("/user/<name>")
